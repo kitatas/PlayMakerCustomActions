10e94d73d50f3aa8dc1aece2495faa086dd28417
ktzwnoy <315kitatas10703@gmail.com>
2019-12-15 02:14:49 +0900

    Add TextMeshPro Actions

---
+++ Assets/PlayMakerCustomActions/TextMeshPro/TextMeshProGetText.cs
@@ -0,0 +1,57 @@
+﻿namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory("TextMeshPro")]
+    [Tooltip("Gets the TMP value of a TMP Text component.")]
+    public class TextMeshProGetText : ComponentAction<TMPro.TextMeshProUGUI>
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(TMPro.TextMeshProUGUI))]
+        [Tooltip("The GameObject with the TextMeshProUGUI component.")]
+        public FsmOwnerDefault gameObject;
+
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        [Tooltip("The text value of the TextMeshProUGUI component.")]
+        public FsmString text;
+
+        [Tooltip("Runs every frame. Useful to animate values over time.")]
+        public bool everyFrame;
+
+        private TMPro.TextMeshProUGUI tmpText;
+
+        public override void Reset()
+        {
+            text = null;
+            everyFrame = false;
+        }
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (UpdateCache(go))
+            {
+                tmpText = cachedComponent;
+            }
+
+            DoGetTextValue();
+
+            if (!everyFrame)
+            {
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            DoGetTextValue();
+        }
+
+        private void DoGetTextValue()
+        {
+            if (tmpText != null)
+            {
+                text.Value = tmpText.text;
+            }
+        }
+    }
+}
---
+++ Assets/PlayMakerCustomActions/TextMeshPro/TextMeshProSetText.cs
@@ -0,0 +1,80 @@
+﻿namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory("TextMeshPro")]
+    [Tooltip("Sets the TMP value of a TMP Text component.")]
+    public class TextMeshProSetText : ComponentAction<TMPro.TextMeshProUGUI>
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(TMPro.TextMeshProUGUI))]
+        [Tooltip("The GameObject with the TextMeshProUGUI component.")]
+        public FsmOwnerDefault gameObject;
+
+        [UIHint(UIHint.TextArea)]
+        [Tooltip("The text of the TextMeshProUGUI component.")]
+        public FsmString text;
+
+        [Tooltip("Reset when exiting this state.")]
+        public FsmBool resetOnExit;
+
+        [Tooltip("Repeats every frame")] 
+        public bool everyFrame;
+
+        private TMPro.TextMeshProUGUI tmpText;
+        private string originalString;
+
+        public override void Reset()
+        {
+            gameObject = null;
+            text = null;
+            resetOnExit = null;
+            everyFrame = false;
+        }
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (UpdateCache(go))
+            {
+                tmpText = cachedComponent;
+            }
+
+            originalString = tmpText.text;
+
+            DoSetTextValue();
+
+            if (!everyFrame)
+            {
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            DoSetTextValue();
+        }
+
+        private void DoSetTextValue()
+        {
+            if (tmpText == null) return;
+
+            tmpText.text = text.Value;
+        }
+
+        public override void OnExit()
+        {
+            if (tmpText == null) return;
+
+            if (resetOnExit.Value)
+            {
+                tmpText.text = originalString;
+            }
+        }
+
+#if UNITY_EDITOR
+        public override string AutoName()
+        {
+            return "UISetText : " + ActionHelpers.GetValueLabel(text);
+        }
+#endif
+    }
+}
