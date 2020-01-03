7155cb54114a9221f62e6b1b6e83c5067739ec08
ktzwnoy <315kitatas10703@gmail.com>
2020-01-03 17:33:50 +0900

    Add UnityRoomTweet

---
+++ Assets/PlayMakerCustomActions/UnityRoom/UnityRoomTweet.cs
@@ -0,0 +1,29 @@
+﻿namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory("UnityRoom")]
+    public class UnityRoomTweet : FsmStateAction
+    {
+        [RequiredField]
+        [Tooltip("The game id on the unityroom.")]
+        public string gameId;
+
+        [RequiredField]
+        [Tooltip("Set Tweet text.")]
+        public FsmString tweetText;
+
+        [Tooltip("Set hash tag name.")]
+        public string[] hashTags;
+
+        public override void Reset()
+        {
+            gameId = "";
+            tweetText = "";
+            hashTags = null;
+        }
+
+        public override void OnEnter()
+        {
+            naichilab.UnityRoomTweet.Tweet(gameId, tweetText.Value, hashTags);
+        }
+    }
+}
